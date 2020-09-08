8bb52daf754a435283875c13fdd93d7e1e6ae895
night <1335715927@qq.com>
2020-09-08 11:55:06 +0800

    part of "console", because the console are above the "fs", So i think maybe build "fs" before console more better.

--- kernel/console.c
+++ kernel/console.c
@@ -1,1 +1,33 @@
-// So, it would take some time to build the fs :-)
+// Console input and output.
+// Input is from the keyboard or serial port.
+// Output is written to the screen and serial port.
+//
+// I think maybe build the fs before the console more butter :-)
+
+#include "types.h"
+#include "spinlock.h"
+#include "fs.h"
+#include "x86.h"
+#include "proc.h"
+
+static void consputc(int);
+
+static int paniced = 0;
+
+static struct {
+	struct spinlock lock;
+	int locking;
+} cons;
+
+static int consolewrite(struct inode *ip, char* buf, int n){
+	
+}
+
+void consoleinit(){
+	lockinit(&cons.lock, "console");
+	devsw[CONSOLE].write = consolewrite;
+	devsw[CONSOLE].read = consoleread;
+
+	// "0" mean the frist cpu hand the IRQ(cpuid = 0)
+	ioapicenable(IRQ_KBD, 0);
+}
