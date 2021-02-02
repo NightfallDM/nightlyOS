950a03cc77b49e1b227dbb7ca3701179092ed9c1
night <1335715927@qq.com>
2021-02-02 17:49:46 +0800

    lazy, so .

---
+++ boot/types.h
@@ -0,0 +1,13 @@
+#ifndef _NTDM_TYPES_H
+#define _NTDM_TYPES_H
+
+typedef unsigned char u8;
+typedef char i8;
+typedef unsigned short u16;
+typedef short short i16;
+typedef unsigned int u32;
+typedef int i32;
+typedef unsigned long u64;
+typedef long i64;
+
+#endif // end "_NTDM_TYPES_H"
