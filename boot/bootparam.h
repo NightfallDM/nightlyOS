f9632e0a1b5d37970cd7e29504be5814f718eb5e
night <1335715927@qq.com>
2021-02-04 21:57:33 +0800

    memory detection func

---
+++ boot/bootparam.h
@@ -0,0 +1,17 @@
+#ifndef _NTDM_BOOTPARAM_H
+#define _NTDM_BOOTPARAM_H
+
+#include"types.h"
+
+struct e820_entry{
+	u64 address;
+	u64 size;
+	u32 type;
+}__attribute__((packed));
+
+struct boot_params{
+	u8 e820_entries;
+	struct e820_entry* e820_table;
+}
+
+#endif // end of "_NTDM_BOOTPARAM_H"
