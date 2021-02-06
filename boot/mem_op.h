0b1336a95dbe8300bb7507ff311c9971d3f67bc5
night <1335715927@qq.com>
2021-02-06 16:31:11 +0800

    the implemention of "memory" opreation.

---
+++ boot/mem_op.h
@@ -0,0 +1,7 @@
+#ifndef _NTDM_MEM_OP_H
+#define _NTDM_MEM_OP_H
+
+extern void memcpy(void* dest, void* source, unsigned int size);
+extern void memset(void* dest, char value, unsigned int size);
+#endif // end of "_NTDM_MEM_OP_H"
+
