44226c6a6b14ce730b390ed19c5279f9ca6be87d
night <1335715927@qq.com>
2021-02-06 16:33:10 +0800

    complete switch to protected mode.

--- boot/bootparam.h
+++ boot/bootparam.h
@@ -3,6 +3,11 @@
 
 #include"types.h"
 
+struct setup_header{
+	u32 kernel_size;
+	u32 code32_start;		
+}
+
 struct e820_entry{
 	u64 address;
 	u64 size;
@@ -12,6 +17,7 @@ struct e820_entry{
 struct boot_params{
 	u8 e820_entries;
 	struct e820_entry* e820_table;
+	struct setup_header* setup_hdr;
 }__attribute__((packed));
 
 #endif // end of "_NTDM_BOOTPARAM_H"
--- boot/main.c
+++ boot/main.c
@@ -1,10 +1,21 @@
 #include"bootparam.h"
 #include"heap.h"
 #include"types.h"
+#include"mem_op.h"
 /* use in asm */
 struct boot_param __boot_param;
 
 struct heap_hdr __hp_hdr;
+
+/*
+ * copy the "setup_header" to "__boot_param" in heap.
+ * */
+void copy_setup_header(void){
+	u16 heap_addr = get_heap(&__hp_hdr, sizeof(struct setup_header));
+	memcpy(heap_addr, &hdr, sizeof(hdr));
+	__boot_param.setup_hdr = (struct setup_header *)heap_addr;
+}
+
 void main(){
 
 	// 10k place: 2k code, 8k heap
@@ -13,6 +24,9 @@ void main(){
 	__boot_param.e820_entry = (struct e820_entry*)0x8600;
 	// do not know how many entries at begin, so use then alloc.
 	u16 _temp = get_heap(&__hp_hdr, 20*__boot_param.e820_entries);
+
+	// copy "hdr" to "__boot_param"
+	copy_setup_header();
 
 	switch_protected_mode();
 }
